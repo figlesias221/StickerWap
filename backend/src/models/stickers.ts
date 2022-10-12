a74d5976967c9bf60b21985729627eb68372a61a
figlesias221 <figlesias221@gmail.com>
2022-10-11 22:34:49 -0300

    fixes

--- backend/src/models/stickers.ts
+++ backend/src/models/stickers.ts
@@ -5,8 +5,7 @@ let mongoose = require("mongoose");
 const stickerSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
-  country: { type: String, required: true },
-  count: { type: Number, required: true },
+  category: { type: String, required: true },
 });
 
 const Sticker = mongoose.model("Sticker", stickerSchema);
